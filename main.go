3802f6817dcea4c980c711cae6f61f3b1ac650e7
Sergey <110046935+SushenaAr@users.noreply.github.com>
2025-07-25 19:31:17 +0300

    remove comments

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"awesomeProject1/internal/runFunc"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "pm",
+	Short: "Packet Manager CLI",
+}
+
+var createCmd = &cobra.Command{
+	Use:   "create <file>",
+	Short: "Создать архив из описания",
+	Args:  cobra.ExactArgs(1),
+	Run:   runFunc.Create,
+}
+
+var updateCmd = &cobra.Command{
+	Use:   "update <file>",
+	Short: "Загрузить и распаковать пакеты",
+	Args:  cobra.ExactArgs(1),
+	Run:   runFunc.Update,
+}
+
+// Мне стало интересно написать пакетный менелджер с полного 0
+func main() {
+	rootCmd.AddCommand(createCmd, updateCmd)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println("Ошибка:", err)
+		os.Exit(1)
+	}
+}
